2a6158996233e134265d534ec50807e05464bea8
bigdomtheexpert <domenico.frigido@gmail.com>
2021-02-06 20:36:46 +0000

    Initial commit

---
+++ lists.py
@@ -0,0 +1,28 @@
+list = ["Andrea" ,"Angense"]
+
+last_person = list.pop()
+print(f"the last person on the list is {last_person.title()}.")
+
+
+popped_list = list.pop()
+print(list)
+print(popped_list)
+
+
+list.append('Greta')
+print(list)
+
+
+list.insert(3,'Dom')
+print(list)
+
+list.append('Eva')
+print(list)
+
+del list[4]
+print(list)
+
+
+
+
+
