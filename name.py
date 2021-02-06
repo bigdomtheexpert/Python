2763f66cfdc6a1e24c7de5bcdd749144016f9906
bigdomtheexpert <domenico.frigido@gmail.com>
2021-02-06 20:48:07 +0000

    Update name.py

--- name.py
+++ name.py
@@ -2,7 +2,7 @@ first_name = "albert"
 last_name = "einstine"
 full_name = f" {first_name} {last_name}"
 message = f",{full_name.title()},once said, if you've never made a mistake in your life you've never tried anything new!"
-print(message)
+print( message )
 
 
 
