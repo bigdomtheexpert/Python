e4ce1c89fd63d171a1f1e725c549fbf7c811409f
bigdomtheexpert <domenico.frigido@gmail.com>
2021-03-17 18:19:14 +0000

    Update if-pizza toppings.py

--- if-pizza toppings.py
+++ if-pizza toppings.py
@@ -1,11 +1,15 @@
-requested_toppings = [ 'mushrooms', 'extra cheese']
 
-if 'mushrooms' in requested_toppings:
-     print("Adding toppings.")
-if 'pepperoni' in requested_toppings:
-    print("Adding pepperoni.")
-if 'extra cheese' in requested_toppings:
-    print("Adding extra cheese.")
+available_toppings = [',mushrooms', 'olives', 'green peppers'
+                      , 'pepperone', 'pineapple', 'extra cheese']
 
+
+requested_toppings = [ 'pepperone', 'french fries', 'extra cheese']
+
+for requested_toppings in requested_toppings:
+    if requested_toppings in available_toppings:
+        print(f"Adding {requested_toppings}.")
+    else:
+        print(f"Sorry we are out of {requested_toppings}.")
+    
 print("\nFinished making your pizza!")
     
